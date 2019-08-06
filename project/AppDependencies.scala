d3504127024c93534b912bb28f78318d10c391a0
Matthew McNally <matthew.mcnally@digital.hmrc.gov.uk>
2019-08-06 16:04:16 +0100

    DDCNLS-6575: Updated external libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,10 +6,10 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "simple-reactivemongo" % "7.20.0-play-25",
-    "org.reactivemongo" %% "reactivemongo-iteratees" % "0.16.1",
+    "org.reactivemongo" %% "reactivemongo-iteratees" % "0.16.4",
     "uk.gov.hmrc" %% "microservice-bootstrap" % "10.6.0",
     "uk.gov.hmrc" %% "domain" % "5.6.0-play-25",
-    "com.typesafe.akka" %% "akka-contrib" % "2.4.10",
+    "com.typesafe.akka" %% "akka-contrib" % "2.5.23",
     "uk.gov.hmrc" %% "play-scheduling" % "6.0.0",
     "uk.gov.hmrc" %% "mongo-lock" % "6.15.0-play-25",
     "uk.gov.hmrc" %% "reactive-circuit-breaker" % "3.3.0",
@@ -19,14 +19,14 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "hmrctest" % "3.9.0-play-25",
-    "org.scalatest" %% "scalatest" % "3.0.2",
+    "org.scalatest" %% "scalatest" % "3.0.8",
     "org.scalamock" %% "scalamock" % "3.6.0",
     "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1",
     "org.pegdown" % "pegdown" % "1.6.0",
     "uk.gov.hmrc" %% "reactivemongo-test" % "4.15.0-play-25",
     "org.reactivemongo" %% "reactivemongo-iteratees" % "0.16.1",
-    "com.typesafe.akka" %% "akka-testkit" % "2.4.10",
-    "org.mockito" % "mockito-core" % "1.9.5",
+    "com.typesafe.akka" %% "akka-testkit" % "2.5.23",
+    "org.mockito" % "mockito-all" % "1.9.5",
     "uk.gov.hmrc" %% "tax-year" % "0.6.0"
   ).map(_ % "test")
 
