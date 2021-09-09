1562756b07232a7aa432d99571ad19480894c2d4
sureshhmrc <27204388+sureshhmrc@users.noreply.github.com>
2021-09-09 15:47:48 +0100

    Suresh's changes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,16 @@ import play.core.PlayVersion
 
 object AppDependencies {
 
+  val playVersion = "play-28"
+  val akkaVersion = "2.6.14"
+
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"         %% "simple-reactivemongo"       % "8.0.0-play-28",
+    "uk.gov.hmrc"         %% "simple-reactivemongo"       % s"8.0.0-$playVersion",
     "org.reactivemongo"   %% "reactivemongo-iteratees"    % "1.0.4",
     "uk.gov.hmrc"         %% "bootstrap-backend-play-28"  % "5.12.0",
-    "uk.gov.hmrc"         %% "domain"                     % "6.2.0-play-28",
-    "uk.gov.hmrc"         %% "play-scheduling"            % "7.4.0-play-26",
-    "uk.gov.hmrc"         %% "mongo-lock"                 % "7.0.0-play-28",
+    "uk.gov.hmrc"         %% "domain"                     % s"6.2.0-$playVersion",
+    "uk.gov.hmrc"         %% "mongo-lock"                 % s"7.0.0-$playVersion",
     "uk.gov.hmrc"         %% "reactive-circuit-breaker"   % "3.5.0",
     "uk.gov.hmrc"         %% "tax-year"                   % "1.4.0",
     "com.typesafe.play"   %% "play-json-joda"             % "2.9.2",
@@ -19,30 +21,18 @@ object AppDependencies {
     compilerPlugin("com.github.ghik" % "silencer-plugin"  % "1.7.5" cross CrossVersion.full)
   )
 
-  trait TestDependencies {
-    lazy val scope: String = "test,it"
-    lazy val test: Seq[ModuleID] = ???
-  }
-  object Tests {
-    def apply(): Seq[ModuleID] = new TestDependencies {
-      override lazy val test: Seq[ModuleID] = Seq(
-//        "uk.gov.hmrc"             %% "service-integration-test"   % "1.1.0-play-28",
-        "org.pegdown"             %  "pegdown"                    % "1.6.0",
-
-//        "uk.gov.hmrc.mongo"     %% "hmrc-mongo-test-play-28"  % "0.36.0",
-        "uk.gov.hmrc"           %% "bootstrap-test-play-28"   % "5.3.0",
-      "com.typesafe.akka"     %% "akka-testkit"             % PlayVersion.akkaVersion,
-        "com.typesafe.akka" %% "akka-slf4j"  % PlayVersion.akkaVersion,
-
-      "com.github.tomakehurst"  %  "wiremock-jre8"               % "2.27.1"         % "test",
-      "uk.gov.hmrc"             %% "reactivemongo-test"       % "5.0.0-play-28"        % "test",
-      "org.mockito"             %  "mockito-all"              % "1.10.19"               % "test",
-      "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.0.0"                 % "test"
-
+ val test: Seq[ModuleID] = Seq(
+      "uk.gov.hmrc"             %% s"bootstrap-test-$playVersion"   % "5.3.0" % "test",
+      "com.typesafe.akka"       %% "akka-testkit"                   % akkaVersion % "test",
+      "com.github.tomakehurst"  %  "wiremock-jre8"                  % "2.27.1"         % "test",
+      "uk.gov.hmrc"             %% "reactivemongo-test"             % s"5.0.0-$playVersion"        % "test",
+      "org.mockito"             %  "mockito-all"                    % "1.10.19"               % "test",
+      "org.scalatestplus.play"  %% "scalatestplus-play"             % "5.1.0"                 % "test",
+      "org.scalatestplus"       %% "scalatestplus-mockito"          % "1.0.0-M2"              % "test",
+      "com.typesafe.play"       %% "play-test"                      % PlayVersion.current      % "test",
+      "com.vladsch.flexmark"    % "flexmark-all"              % "0.35.10"               % "test"
       )
-    }.test
-  }
 
-  val all: Seq[ModuleID] = compile ++ Tests()
+  val all: Seq[ModuleID] = compile ++ test
 
 }
